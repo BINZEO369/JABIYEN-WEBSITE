135a20b445bb67263ffa98ce6e9e6ca39431ba33
BINZEO.IOT <binzeo369@gmail.com>
2026-08-06 08:02:10 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
